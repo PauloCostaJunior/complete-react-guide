53c4f8e8f98b78928464ae52c2691ce52368ae75
Paulo Costa Jr <paulocostajr.@Paulos-MacBook-Air.local>
2018-12-22 18:00:49 -0200

    Lesson69

---
+++ src/containers/App.js
@@ -0,0 +1,121 @@
+import React, { PureComponent } from 'react';
+
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
+//import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('[App.js]', props);
+    this.state = {
+      persons: [
+        { id: '1', name: 'Max', age: 28 },
+        { id: '2', name: 'Manu', age: 29 },
+        { id: '3', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value',
+      showPersons: false
+    };
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()');
+  }
+
+  /*
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return nextState.persons !== this.state.persons ||
+           nextState.showPersons !== this.state.showPersons;
+  }*/
+
+  componentWillUpdate(nextProps, nextState) {
+      console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
+  /*state = {
+    persons: [
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }*/
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id ===  id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //const person = Object.assign({}, this.this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    this.setState( {persons: persons})
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    //update state this way - create a copy - change that and update with setState
+    const persons = [...this.state.persons];
+    persons.splice( personIndex, 1 ); //remove one element from the array
+    this.setState( {persons: persons} );
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    //changes doesShow value
+    this.setState( {showPersons: !doesShow} ) ;
+  }
+
+  render () {
+    console.log('[App.js] Inside render');
+    let persons = null;
+    
+    //if this.state.showPersons is true
+
+    if ( this.state.showPersons ) {
+      persons = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />;  
+    }
+
+    //let classes = ['red', 'bold'].join(' ');
+    
+
+    return (
+        <Aux>
+          <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
+            <Cockpit 
+              showPersons={this.state.showPersons}
+              persons={this.state.persons}
+              clicked={this.togglePersonsHandler} />
+            {persons}
+        </Aux>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default withClass(App, classes.App);
